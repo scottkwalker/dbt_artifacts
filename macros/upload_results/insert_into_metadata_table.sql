ad94582f16e6419cd80fdfbd168c3c152725e120
Scott Walker <scott.walker@troweprice.com>
2025-01-09 16:42:32 +0000

    DBT 1.8.0 for Dremio

--- macros/upload_results/insert_into_metadata_table.sql
+++ macros/upload_results/insert_into_metadata_table.sql
@@ -70,6 +70,15 @@
 
 {%- endmacro %}
 
-{% macro default__insert_into_metadata_table(relation, fields, content) -%}
+{% macro dremio__insert_into_metadata_table(relation, fields, content) -%}
+    {% set insert_into_table_query %}
+    insert into {{ relation }}
+    values
+    {{ content }}
+    {% endset %}
+
+    {% do run_query(insert_into_table_query) %}
 {%- endmacro %}
 
+{% macro default__insert_into_metadata_table(relation, fields, content) -%}
+{%- endmacro %}
